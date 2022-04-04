c6bfd8e77405d502350b8938d026bcc2268cb9c0
Rani Uttam Kamble <98833984+rani-ukamble@users.noreply.github.com>
2022-04-04 19:00:35 +0530

    Create input_output.cpp

---
+++ input_output.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    scanf("%d %ld %c %f %lf",&a,&b,&c,&d,&e); //scanf printf are fast than cin cout
+    printf("%d\n%ld\n%c\n%f\n%lf",a,b,c,d,e);
+    // Complete the code.
+    return 0;
+}
